a16b67df61e27a5fa4fab1332db176d3c3b76a02
komalshinde16 <komalambadasshinde@gmail.com>
2026-04-02 10:52:36 +0530

    sum

--- src/main/java/com/itshaala/Array_Java/SumofArrays.java
+++ src/main/java/com/itshaala/Array_Java/SumofArrays.java
@@ -1,11 +1,13 @@
 package com.itshaala.Array_Java;
 
-public class PrintArray {
+public class SumofArrays {
     public static void main(String[] args) {
         int[] array = {10,20,30,40,50,60,70};
+        int sum = array[0];
 
         for(int i=0; i< array.length; i++){
-            System.out.print(array[i]+ " ");
+            sum = sum + array[i];
         }
+        System.out.println("sum of the arrays is: " + sum);
     }
 }
